6fd470036231abba9d70196edbbae6778752b337
zvanderbilt <zack@kiosk.tm>
2016-04-08 14:51:48 -0700

    test3

--- auto_rawl.sh
+++ auto_rawl.sh
@@ -7,11 +7,11 @@ if [ -z $recipient ]
 	then echo "Aborting, no recipient specified..."
 	     echo ""
 	     echo "Useage: $0 [site-to-crawl] [recipient of alerts]" 
-	exit 
+	exit 1 
 elif [[ $cmd == *200* ]]
 	then echo "$cmd" | mail -s "404's found on $1" "$2"
 else
 	echo "Congrats! No 404's found!" | mail -s "No 404's on $1" "$2"
 fi
 
-exit
+exit 0
