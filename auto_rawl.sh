5171602f2ea56ba6bdd1a93985a5c31d478b6942
zvanderbilt <zack@kiosk.tm>
2016-04-08 14:36:47 -0700

    test

--- auto_rawl.sh
+++ auto_rawl.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-cmd = $(rawler "$1" --wait 1 | grep 200)
+recipient="$2"
+cmd=$(rawler "$1" --wait 1 | grep 200)
 
-if [[ $cmd == *200* ]]
+if [[ -z $recipient]];
+	then echo "Aborting, no recipient specified..."
+	     echo ""
+	     echo "Useage: $0 [site-to-crawl] [recipient of alerts]"; 
+	exit 0
+elif [[ $cmd == *200* ]];
 	then echo "$cmd" | mail -s "404's found on $1" $2
 else
 	echo "Congrats! No 404's found!" | mail -s "No 404's on $1" $2
