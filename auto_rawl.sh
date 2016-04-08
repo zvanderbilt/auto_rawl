4ad7742c32cdd58478c482b43d415746cdf98d98
zvanderbilt <zack@kiosk.tm>
2016-04-08 16:21:53 -0700

    working

--- auto_rawl.sh
+++ auto_rawl.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 cmd=$(rawler "$1" --wait 1 | grep 200)
-
 if [ -z $1 ]
 	then echo "Aborting, no target specified..."
 	     echo ""
 	     echo "Useage: $0 [site-to-crawl]" 
 elif [[ $cmd == *200* ]]
-	then echo "$cmd" >> broke 
+	then readarray -t array <<< "$cmd"
+	showme=$(printf -- "%s\n" "${array[@]}")
+	broke=$(echo $showme | awk ' {print $NF} ')
+	echo "Boo, 404s found on $broke"
+	echo "$showme"
 else
 	echo "Congrats! No 404's found!" 
 fi
+
+
