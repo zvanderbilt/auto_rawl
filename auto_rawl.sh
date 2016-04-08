c02ea379b6974ca95e78432648182cf4849d155d
zvanderbilt <zack@kiosk.tm>
2016-04-08 14:55:23 -0700

    test_final

--- auto_rawl.sh
+++ auto_rawl.sh
@@ -9,7 +9,7 @@ if [ -z $recipient ]
 	     echo "Useage: $0 [site-to-crawl] [recipient of alerts]" 
 	exit 1 
 elif [[ $cmd == *200* ]]
-	then echo "$cmd" | mail -s "404's found on $1" "$2"
+	then echo "$cmd" | mail -s "ALERT: 404's found on $1" "$2"
 else
 	echo "Congrats! No 404's found!" | mail -s "No 404's on $1" "$2"
 fi
