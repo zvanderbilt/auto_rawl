cb2fde2374ec2df1013fe60b617e2abecd329c81
zvanderbilt <zack@kiosk.tm>
2016-04-08 15:14:43 -0700

    test_final3

--- auto_rawl.sh
+++ auto_rawl.sh
@@ -2,15 +2,10 @@
 
 cmd=$(rawler "$1" --wait 1 | grep 200)
 
-if [ -z $recipient ]
-	then echo "Aborting, no recipient specified..."
-	     echo ""
-	     echo "Useage: $0 [site-to-crawl] [recipient of alerts]" 
-	exit 1 
-elif [[ $cmd == *200* ]]
+if [[ $cmd == *200* ]]
 	then echo "$cmd" 
 else
 	echo "Congrats! No 404's found!" 
 fi
 
-exit 0
+exit 
