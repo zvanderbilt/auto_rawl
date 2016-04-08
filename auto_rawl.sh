4b1417b04d9efff2a386951960e6b3b2a6b30f5d
zvanderbilt <zack@kiosk.tm>
2016-04-08 12:29:07 -0700

    added option to specify email

--- auto_rawl.sh
+++ auto_rawl.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-recipient=$2
+recipient=$($2)
 cmd=$(rawler $1 --wait 1 | grep 404)
 
 if [[ $cmd == *404* ]]
-	then echo "$cmd" | mail -s "404's found on $1"  $2
+	then echo "$cmd" | mail -s "404's found on $1"  $recipient
 else
-	echo "Congrats! No 404's found!"
+	echo "Congrats! No 404's found!" | mail -s "No 404's on $1"
 fi
 
 exit 0
