a4d803c356619aecc5c7e89ad36d209cf153e937
zvanderbilt <zack@kiosk.tm>
2016-04-08 14:30:39 -0700

    test

--- auto_rawl.sh
+++ auto_rawl.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-cmd=$(rawler $1 --wait 1 | grep 404)
+cmd = $(rawler "$1" --wait 1 | grep 200)
 
-if [[ $cmd == *404* ]]
+if [[ $cmd == *200* ]]
 	then echo "$cmd" | mail -s "404's found on $1" $2
 else
 	echo "Congrats! No 404's found!" | mail -s "No 404's on $1" $2
 fi
 
 echo "$*"
-
-exit 0
