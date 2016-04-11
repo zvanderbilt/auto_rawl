157f036578b84030870d81e51f8bc90f149295a0
zvanderbilt <zack@kiosk.tm>
2016-04-11 12:40:11 -0700

    added conditional

--- auto_rawl.sh
+++ auto_rawl.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+if [ -a ./output ]
+	then rm -f ./output
+fi
 cmd=$(rawler "$1" --wait 1 | grep 200)
 if [ -z $1 ]
 	then echo "Aborting, no target specified..."
 	     echo ""
 	     echo "Useage: $0 [site-to-crawl]" 
+	exit 1
 elif [[ $cmd == *200* ]]
 	then readarray -t array <<< "$cmd"
 	showme=$(printf -- "%s\n" "${array[@]}")
 	broke=$(echo $showme | awk ' {print $NF} ')
 	echo "Boo, 404s found on $broke"
 	echo "$showme"
-elif [[ $cmd != *40?* ]]
+elif [[ $cmd != *40[1-9]* ]]
         then readarray -t array <<< "$cmd"
         showme=$(printf -- "%s\n" "${array[@]}")
         notbroke=$(echo $showme | awk ' {print $NF} ')
