b6f16ec6633d55e590f8ced941b09ece59968609
zvanderbilt <zack@kiosk.tm>
2016-04-11 14:41:32 -0700

    working draft

--- auto_rawl.sh
+++ auto_rawl.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-cmd=$(rawler "$1" --wait 1 | grep 200)
+cmd=$(rawler "$1" --wait 1 | grep 404)
 if [ -z $1 ]
 	then echo "Aborting, no target specified..."
 	     echo ""
 	     echo "Useage: $0 [site-to-crawl]" 
 	exit 1
-elif [[ $cmd == *200* ]]
+elif [[ $cmd == *404* ]]
 	then readarray -t array <<< "$cmd"
 	showme=$(printf -- "%s\n" "${array[@]}")
 	broke=$(echo $showme | awk ' {print $NF} ')
 	echo "Boo, 404s found on $broke"
 	echo "$showme"
 elif [[ $cmd != 404 ]]
-        then readarray -t array2 <<< "$cmd"
-        showme=$(printf -- "%s\n" "${array[@]}")
-        notbroke=$(echo $showme | awk ' {print $NF} ')
+        notbroke=$(echo $cmd | awk ' {print $1} ')
 	echo "Congrats! No 404's found on $notbroke" 
 fi
 
