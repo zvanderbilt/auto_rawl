30653e4d6fe4bafa8d5f7d7c28eb5eb42b30a49c
zvanderbilt <zack@kiosk.tm>
2016-04-12 09:48:48 -0700

    simplified conditionals

--- auto_rawl.sh
+++ auto_rawl.sh
@@ -6,15 +6,13 @@ if [ -z $1 ]
 	     echo ""
 	     echo "Useage: $0 [site-to-crawl]" 
 	exit 1
-elif [[ $cmd == *404* ]]
-	then readarray -t array <<< "$cmd"
+elif [[ $cmd == *404* ]]; then
+	readarray -t array <<< "$cmd"
 	showme=$(printf -- "%s\n" "${array[@]}")
 	broke=$(echo $showme | awk ' {print $NF} ')
 	echo "Boo, 404s found on $broke"
 	echo "$showme"
-elif [[ $cmd != 404 ]]
+else
         notbroke=$(echo $cmd | awk ' {print $1} ')
-	echo "Congrats! No 404's found on $notbroke" 
+	echo "Congrats! No 404's found on $notbroke"
 fi
-
-
